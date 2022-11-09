dda125904bf1fbbc1bbe799f5b0c4a067ef7c2e3
vlad <estevez.vladimir@yahoo.com>
2022-11-09 09:50:22 -0500

    Changed the attributes access from public to private and added code that makes the class work

--- OOP2Test2Program/Length.cs
+++ OOP2Test2Program/Length.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace OOP2Test2Program
 {
@@ -11,20 +12,54 @@ namespace OOP2Test2Program
     /// </summary>
     public class Length
     {
+        //Changed the access level of the attributes "value" and unit, so they follow the encapsulation concept
+        //which protects valuable information that we do not want to be accesible by the end-user.
+        //Also added a lower dash to the name of the fields so they are not mistaken by other important keywords
+        //Since using a keyword as an identifier is a syntax error
+
         /// <summary>
         /// Field saving the length. Negative values are not permitted.
         /// </summary>
-        public int value;
+        private int _value;
 
         /// <summary>
         /// Field saving the unit. Only use "m" and "ft" as values.
         /// </summary>
-        public string unit;
+        private string _unit;
 
   /// <inheritdoc/>
         public override string ToString()
         {
-            return value.ToString();
+            return _value.ToString();
         }
-    }
+        /// <summary>
+        /// gets and sets the value of the field _value. It only accepts values that are positive.
+        /// </summary>
+        public int Value {
+            get { return _value; }
+            set { if(_value >0) { _value = value; }
+                else {
+                    MessageBox.Show("Only positive numbers are allowed");
+                        } 
+                 }
+        }
+        /// <summary>
+        /// 
+        /// gets and sets the value of the field _unit. Only ft and m values are allowed.
+        /// </summary>
+        public string Unit { 
+
+            get {
+                return _unit;
+                 }
+            set {
+                if (_unit.Contains("m") || _unit.Contains("ft")){
+                     _unit = value;
+                }
+                else
+                {
+                    MessageBox.Show("only m and ft measures are allowed");
+                }
+  } }
+        }
 }
